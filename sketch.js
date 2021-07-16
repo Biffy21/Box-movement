db926e683c6b72042600c0a481a87ee75b9300b4
Biffy21 <87482430+Biffy21@users.noreply.github.com>
2021-07-16 12:42:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,7 +3,6 @@ function setup() {
   createCanvas(400,400);
 box= createSprite(200,200,30,30);
 }
-
 function draw() 
 {
   background(30);
@@ -13,6 +12,10 @@ function draw()
   if(keyIsDown(LEFT_ARROW)){
     box.position.x=box.position.x-5;
   }
+  if(keyIsDown(DOWN_ARROW)){
+    background("blue");
+  }
+
 drawSprites();
 }
 
